c778f8881a9416e0a6b7fa20155dd44aee93fc99
abdullah <abdullah.kayesh@gmail.com>
2024-07-17 12:11:56 +0600

    remove tranpilation in favour of compileoption

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 // build command ..... npx rollup -c rollup.config.js
-// Transpile to ES5 .. tsc --out ./dist/vendor.js --target es5 --allowJs dist/x52-type-def.js
 // npm publish ... npm publish
 
 
